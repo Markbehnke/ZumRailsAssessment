c63a84b037245db7ed047574d956de2b1533e0d1
markbehnke <mark.behnke1998@gmail.com>
2024-11-18 13:50:16 -0800

    Moved gitignore

--- PokemonWebAPI/obj/Debug/net8.0/PokemonWebAPI.AssemblyInfo.cs
+++ PokemonWebAPI/obj/Debug/net8.0/PokemonWebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03b27d2d97c211ce25dbf178f18534c7cd44b45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd1761f64052d89473efb73157798e5f1363524")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
